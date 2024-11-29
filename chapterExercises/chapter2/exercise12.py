797fb585ac05b91a3ae312c62b34dae0d00b298f
0nyinye <onyinyeekezi2017@gmail.com>
2024-11-29 18:49:08 +0100

    update after taken down

---
+++ chapterExercises/chapter2/exercise12.py
@@ -0,0 +1,18 @@
+principal=1000
+annualRate=7/100
+numberOfYears=10
+yearTen=1000*(1+0.07)**10
+
+numberOfYears=20
+yearTwenty=1000*(1+0.07)**20
+
+numberOfYears=30
+yearThirty=1000*(1+0.07)**30
+
+print('Amount in ten years is: ', yearTen)
+print('Amount in twenty years is: ', yearTwenty)
+print('Amount in thirty years is: ', yearThirty)
+
+
+
+
